c846a51d8672c0bd3599b0b54c59e24f599ee6f6
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-03-30 16:11:52 +0100

    Add DisposableEmailDetector class tests

---
+++ test/DisposableEmailDetectorTest.cs
@@ -0,0 +1,41 @@
+namespace ChatAIze.PerfectEmail.Test;
+
+[TestClass]
+public sealed class DisposableEmailDetectorTest
+{
+    [DataTestMethod]
+    [DataRow(null, false)]
+    [DataRow("", false)]
+    [DataRow("someone", false)]
+    [DataRow("gmail.com", false)]
+    [DataRow("@gmail.com", false)]
+    [DataRow("  @gmail.com", false)]
+    [DataRow("@gmail.com  ", false)]
+    [DataRow("0-mail.com", true)]
+    [DataRow("  0-mail.com", true)]
+    [DataRow("0-mail.com  ", true)]
+    [DataRow("@0-mail.com", true)]
+    [DataRow("@0-maIl.Com", true)]
+    [DataRow("someone@gmail.com", false)]
+    [DataRow("someone@0-mail.com", true)]
+    public void IsDisposableEmailTest(string? email, bool expected)
+    {
+        var result = DisposableEmailDetector.IsDisposableEmail(email);
+        Assert.AreEqual(result, expected);
+    }
+
+    [DataTestMethod]
+    [DataRow(null, false)]
+    [DataRow("", false)]
+    [DataRow("x", false)]
+    [DataRow("gmail", false)]
+    [DataRow("gmail.com", false)]
+    [DataRow("gMail.cOm", false)]
+    [DataRow("0-mail.com", true)]
+    [DataRow("0-MaIL.cOm", true)]
+    public void IsDisposableEmailDomainTest(string? email, bool expected)
+    {
+        var result = DisposableEmailDetector.IsDisposableEmailDomain(email);
+        Assert.AreEqual(result, expected);
+    }
+}
