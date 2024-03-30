008d28c653e4dde6c99f30088be29d843bd6a63e
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-03-30 17:27:33 +0100

    Add push_nuget.sh script

---
+++ tools/push_nuget.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ../src
+
+dotnet build
+dotnet publish
+dotnet pack
+
+latest_package=$(ls ./bin/Release/ChatAIze.PerfectEmail.*.*.*.nupkg | sort -V | tail -n 1)
+dotnet nuget push "$latest_package" --api-key $CHATAIZE_NUGET_API_KEY --source https://api.nuget.org/v3/index.json
