1822f55162ef63acea2f126fc7239bfcc62aa7bc
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-10-01 00:13:56 +0200

    Add email normalizer tests

---
+++ test/EmailNormalizerTest.cs
@@ -0,0 +1,38 @@
+namespace ChatAIze.PerfectEmail.Test
+{
+    [TestClass]
+    public sealed class EmailNormalizerTest
+    {
+        [DataTestMethod]
+        [DataRow(null, null)]
+        [DataRow("", null)]
+        [DataRow(" ", null)]
+        [DataRow("a", null)]
+        [DataRow("@", null)]
+        [DataRow("a@", null)]
+        [DataRow("@a", null)]
+        [DataRow("a@a", null)]
+        [DataRow("a@a.a", null)]
+        [DataRow(".", null)]
+        [DataRow("a.", null)]
+        [DataRow(".a", null)]
+        [DataRow("a.a", null)]
+        [DataRow("@a.a", null)]
+        [DataRow("a@a.aa", "a@a.aa")]
+        [DataRow(" a@a.aa", "a@a.aa")]
+        [DataRow("a@a.aa ", "a@a.aa")]
+        [DataRow(" a@a.aa ", "a@a.aa")]
+        [DataRow("a+a@a.aa", "a@a.aa")]
+        [DataRow(" a+a@a.aa", "a@a.aa")]
+        [DataRow("a+a@a.aa ", "a@a.aa")]
+        [DataRow(" a+a@a.aa ", "a@a.aa")]
+        [DataRow("a+aa@a.aa", "a@a.aa")]
+        [DataRow("aa+a@a.aa", "aa@a.aa")]
+        [DataRow("aa+aa@a.aa", "aa@a.aa")]
+        public void NormalizeEmailTest(string email, object expected)
+        {
+            var result = EmailNormalizer.NormalizeEmail(email);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
